5f886f2f0fa70b76a7a2546167c6f37d832bbee9
Lucas Resch <lucas.resch@gmx.de>
2021-12-31 15:52:46 +0100

    ✨ Implement basic cookiecutter template
    This template bootstraps the creation of new DAGOS ansible roles.

---
+++ README.md
@@ -0,0 +1,11 @@
+# cookiecutter-ansible-role
+
+This repository provides a simple [Cookiecutter](https://github.com/cookiecutter/cookiecutter) template for bootstrapping DAGOS Ansible roles.
+
+## Dependencies
+
+Install the following dependencies first:
+
+* [Cookiecutter](https://github.com/cookiecutter/cookiecutter)
+* [Molecule](https://molecule.readthedocs.io/en/latest/)
+* [pre-commit](https://pre-commit.com/)
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,28 @@
+import subprocess
+
+# Create an initial commit using emojis from https://gitmoji.dev/
+subprocess.run(["git", "init"])
+subprocess.run(["git", "add", "LICENSE"])
+subprocess.run(["git", "commit", "-m", "🎉 Initial commit"])
+
+# Initialize Ansible role via Molecule.
+ansible_role = "{{ cookiecutter.role_namespace }}.{{ cookiecutter.role_name }}"
+subprocess.run(
+    [
+        "molecule",
+        "init",
+        "role",
+        ansible_role,
+        "--driver-name",
+        "{{ cookiecutter.molecule_driver }}",
+    ]
+)
+subprocess.run(
+    ["rm", f"{ansible_role}/README.md", f"{ansible_role}/molecule/default/molecule.yml"]
+)
+subprocess.run(f"cp -r {ansible_role}/* {ansible_role}/.[!.]* .", shell=True)
+subprocess.run(["rm", "-rf", ansible_role])
+subprocess.run(["rm", ".travis.yml"])
+
+# Install pre-commit hooks
+subprocess.run(["pre-commit", "install"])
